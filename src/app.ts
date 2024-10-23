57e5adaddf45d605d9bd989a539092d29356cda4
ANHTHU-LAPTOP\20521 <trtruong1999@gmail.com>
2024-10-23 16:27:02 +0700

    - completed learning typescript

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+import todosRoutes from './routes/todos'
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(todosRoutes)
+
+app.listen(3000, () => {
+    console.log('Server running at port: 3000')
+});
